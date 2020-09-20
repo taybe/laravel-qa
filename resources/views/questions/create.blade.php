29ec0f61ced37c59db93efb99d6e5a9fcd0f9d06
Taybe <taybemuharem@gmail.com>
2020-09-20 09:33:58 +0100

    Implemented form validation and storing questions.

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -19,7 +19,7 @@
                         @csrf
                         <div class="form-group">
                             <label for="question-title">Question Title</label>
-                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" />
+                            <input type="text" name="title" value="{{ old('title') }}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" />
                             
                             @if ($errors->has('title'))
                                 <div class="invalid-feedback">
@@ -30,7 +30,7 @@
                         </div>
                         <div class="form-group">
                             <label for="question-body">Explain your Question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body') }}</textarea>
                             
                             @if ($errors->has('body'))
                                 <div class="invalid-feedback">
@@ -40,7 +40,7 @@
                             
                         </div>
                         <div class="form-group">
-                            <button type="submit" name="title" class="btn btn-outline-primary btn-lg">Ask This Question</button>
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask This Question</button>
                         </div>
                     </form>
                 </div>

0da3d06bb86405789aac5276d23d5d53c7ecf49a
Taybe <taybemuharem@gmail.com>
2020-09-27 16:21:25 +0100

    Implemented authorization using policies.

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,9 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -68,6 +71,8 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize("update", $question);
+        
         return view("questions.edit", compact('question'));
     }
 
@@ -80,6 +85,8 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize("update", $question);
+        
         $question->update($request->only('title', 'body'));
         
         return redirect('/questions')->with('success', "Your question has been updated.");
@@ -93,6 +100,8 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize("delete", $question);
+        
         $question->delete();
         
         return redirect('/questions')->with('success', "Your question has been deleted.");

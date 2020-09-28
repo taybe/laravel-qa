b40aafbf9e414a3a2f22daf7390a120443d1fdd9
Taybe <taybemuharem@gmail.com>
2020-09-28 19:52:11 +0100

    Created fake answers.

--- app/Answer.php
+++ app/Answer.php
@@ -19,4 +19,13 @@ class Answer extends Model
         
         return $markdown -> convertToHtml($this->body);
     }
+    
+    public static function boot(){
+        
+        parent::boot();
+        
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+        });
+    }
 }

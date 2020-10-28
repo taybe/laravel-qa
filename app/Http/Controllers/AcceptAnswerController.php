802e4d9a47e2f0db21566540171dc1b12ca5afb9
Taybe <taybemuharem@gmail.com>
2020-10-28 17:42:14 +0000

    Created Accept best answer Vue.js component.

--- app/Http/Controllers/AcceptAnswerController.php
+++ app/Http/Controllers/AcceptAnswerController.php
@@ -10,6 +10,13 @@ class AcceptAnswerController extends Controller
     public function __invoke(Answer $answer){
         $this->authorize('accept', $answer);
         $answer->question->acceptBestAnswer($answer);
+        
+        if(request()->expectsJson()){
+            return response()->json([
+                'message' => "You have accepted this answer as best answer."
+            ]);
+        }
+        
         return back();
     }
 }

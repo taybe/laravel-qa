dc7d7386590e33f5eefe176860ac498b2a553207
Taybe <taybemuharem@gmail.com>
2020-11-02 14:32:11 +0000

    Utilizing vue mixins.

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -34,15 +34,17 @@
 <script>
 import Vote from './Vote.vue';
 import UserInfo from './UserInfo.vue';
+import modification from '../mixins/modification';
 
     export default {
         props: ['answer'],
+        
+        mixins: [modification],
         
         components: { Vote, UserInfo },
     
         data(){
             return{
-                editing: false,
                 body: this.answer.body,
                 bodyHtml: this.answer.body_html,
                 id: this.answer.id,
@@ -52,57 +54,23 @@ import UserInfo from './UserInfo.vue';
         },
         
         methods: {
-            edit(){
+            setEditCache(){
                 this.beforeEditCache = this.body;
-                this.editing = true;
             },
-            cancel(){
+            restoreFromCache(){
                 this.body = this.beforeEditCache;
-                this.editing = false;
             },
-            update(){
-                axios.patch(this.endpoint,{
+            payload(){
+                return {
                     body: this.body
-                })
-                .then(res => {
-                    console.log(res);
-                    this.editing = false;
-                    this.bodyHtml = res.data.body_html;
-                    this.$toast.success(res.data.message, "Success", { timeout: 3000 });
-                })
-                .catch(err => {
-                    this.$toast.error(err.data.message, "Error", { timeout: 3000 });
-                });
+                };
             },
-            destroy(){
-                this.$toast.question('Are you sure about that?', "Confirm", {
-                timeout: 20000,
-                close: false,
-                overlay: true,
-                displayMode: 'once',
-                id: 'question',
-                zindex: 999,
-                title: 'Hey',
-                message: 'Are you sure about that?',
-                position: 'center',
-                buttons: [
-                    ['<button><b>YES</b></button>', (instance, toast) => {
-                        axios.delete(this.endpoint)
+            delete(){
+                axios.delete(this.endpoint)
                             .then(res => {
-                                this.$emit('deleted')
+                                this.$toast.success(res.data.message, "Success", { timeout: 3000 });
+                                this.$emit('deleted');
                             });
-                        
-                        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-
-                    }, true],
-                    ['<button>NO</button>', function (instance, toast) {
-
-                        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-
-                    }],
-                ]
-            });
-                
             }
         },
         

17635004f552478454279b1a828b75ca67cb6af3
Taybe <taybemuharem@gmail.com>
2020-11-13 12:44:47 +0000

    Created update and delete question endpoints.

--- app/Http/Controllers/Api/QuestionsController.php
+++ app/Http/Controllers/Api/QuestionsController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\QuestionResource;
+use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
@@ -16,8 +17,8 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        $questions = Question::with('user')->latest()->paginate(5);
-        
+        $questions = Question::with('user')->latest()->paginate(10);
+
         return QuestionResource::collection($questions);
     }
 
@@ -27,9 +28,14 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $question = $request->user()->questions()->create($request->only('title', 'body'));
+
+        return response()->json([
+            'message' => "Your question has been submitted",
+            'question' => new QuestionResource($question)
+        ]);
     }
 
     /**
@@ -40,7 +46,11 @@ class QuestionsController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return response()->json([
+            'title'     => $question->title,
+            'body'      => $question->body,
+            'body_html' => $question->body_html
+        ]);
     }
 
     /**
@@ -50,9 +60,16 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $this->authorize("update", $question);
+
+        $question->update($request->only('title', 'body'));
+
+        return response()->json([
+            'message' => "Your question has been updated.",
+            'body_html' => $question->body_html
+        ]);
     }
 
     /**
@@ -63,6 +80,12 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $this->authorize("delete", $question);
+
+        $question->delete();
+
+        return response()->json([
+            'message' => "Your question has been deleted."
+        ]);
     }
 }

d600366597d38bd32cb4ddd80107848afaa92716
Taybe <taybemuharem@gmail.com>
2020-11-15 09:33:38 +0000

    Created some andpoints for Answer resource.

---
+++ app/Http/Controllers/Api/AnswersController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Answer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Question;
+use App\Http\Resources\AnswerResource;
+
+class AnswersController extends Controller
+{
+    
+    public function index(Question $question){
+        $answers = $question->answers()->with('user')->simplePaginate(3);
+        
+        return AnswerResource::collection($answers);
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Question $question, Request $request)
+    {
+        $request->validate([
+            'body' => 'required'
+        ]);
+        
+        $answer = $question->answers()->create(['body' => $request->body, 'user_id' => \Auth::id()]);
+        
+        return response()->json([
+            'message' => 'Your answer has been submitted successfully.',
+            'answer' => new AnswerResource($answer->load('user'))
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question, Answer $answer)
+    {
+        $this->authorize('update', $answer);
+        
+        $answer->update($request->validate([
+            'body' => 'required',
+        ]));
+        
+        return response()->json([
+            'message' => 'Your answer has been updated.',
+            'body_html' => $answer->body_html
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Answer  $answer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question, Answer $answer)
+    {
+        $this->authorize('delete', $answer);
+        
+        $answer->delete();
+        
+        return response()->json([
+            'message' => "Your answer has been removed."
+        ]);
+    }
+}

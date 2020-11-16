e691e407af4ea99c7ba63cf09ae28aef1224c4f0
Taybe <taybemuharem@gmail.com>
2020-11-16 14:24:48 +0000

    Created api endpoints for vote question and answer.

---
+++ app/Http/Controllers/Api/VoteAnswerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Answer;
+
+class VoteAnswerController extends Controller
+{
+    
+    public function __invoke(Answer $answer){
+        
+        $vote = (int) request()->vote;
+        
+        $votesCount = auth()->user()->voteAnswer($answer, $vote);
+        
+        return response()->json([
+            'message' => "Thanks for the feedback.",
+            'votesCount' => $votesCount
+        ]);
+    }
+}
